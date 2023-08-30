28ae2718572417733f1b144dcb3764a03de9e851
Nite TSAI(蔡奕德) <g-nitetsai@gamania.com>
2023-08-30 11:30:08 +0800

    20230830 update

--- src/App.js
+++ src/App.js
@@ -12,12 +12,21 @@ let isClick = false;
 //   console.log('is clikc?',isClick)
 // }
 const ClickTitle = () => {
-  gsap.to(".menu_title",{duration: 1,x: isClick ? 0 : -790, y:isClick? 0 : -420, onUpdate:function(){
+  gsap.to(".App-header",{duration: 1,x: isClick ? 0 : -790, y:isClick? 0 : -420, onUpdate:function(){
     // document.getElementsByClassName('menu_title')[0].style.fontSize = '50px';
     gsap.to(".menu_title",{duration: 1,scaleX : isClick ? 1 : 0.5,scaleY : isClick ? 1 : 0.5,});
   },onComplete:function(){
+    document.getElementById('tsparticles').style.visibility = isClick ? 'visible' : 'hidden';
+    document.getElementById('sideBtn').style.visibility = isClick ? 'hidden' : 'visible';
+    
     isClick = !isClick;
   }
+  // gsap.to(".menu_title",{duration: 1,x: isClick ? 0 : this.screenX, y:isClick? 0 : -420, onUpdate:function(){
+  //   // document.getElementsByClassName('menu_title')[0].style.fontSize = '50px';
+  //   gsap.to(".menu_title",{duration: 1,scaleX : isClick ? 1 : 0.5,scaleY : isClick ? 1 : 0.5,});
+  // },onComplete:function(){
+  //   isClick = !isClick;
+  // }
 });
   
 };
@@ -26,9 +35,12 @@ function App() {
     <div className="App">
 
       <header className="App-header">
-        <div className='menu_title' onClick={ClickTitle}>Nite's Resume</div>
-        <ParticlesBackground />
+        <div className='menu_title' onClick={ClickTitle}>Nite's Profile</div>
       </header>
+      <div className='sideBtn' id='sideBtn'>
+        btn
+      </div>
+      <ParticlesBackground id='tsparticles'/>
       
     </div>
   );
